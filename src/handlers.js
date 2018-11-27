da02cc07a125c399a0a225c7083fe5052be429c8
amanshawar <amanshawar_a@hotmail.com>
2018-11-27 15:02:47 +0200

    make handler.js file #4

---
+++ src/handlers.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const fs = require('fs');
+const spt = require('/praysearch.js');
+//------------------------------------------------
+const  homeHandler=(request,response)=>{
+  const htmlPath =  path.join(__dirname, '../client/index.html')
+  fs.readFile(htmlPath, (error, html) => {
+      if(error){
+         response.writeHead(500, {'content-Type': 'text/html'})
+         response.end('<h1> Server error! sorry</h1>')
+      }
+      response.writeHead(200, {'Content-Type': 'text/html'})
+      response.end(html)
+  });
+}
+//------------------------------------------------
+const  clientHandler=(request,response)=>{
+  const filePath =  path.join(__dirname, '..', request.url);
+  const extension = request.url.split('.')[1];
+  const contentTypeMapping = {
+     'js': 'application/js',
+     'css': 'text/css',
+     'html': 'text/html'
+     }
+ fs.readFile(filePath, (error, file) => {
+     if(error){
+         response.writeHeader(500, {'content-Type': 'text/css'});
+         response.end('<h1> Server error! sorry</h1>');
+     }
+     response.writeHead(200, {'content-Type': contentTypeMapping[extension]})
+     response.end(file)
+ })
+}
+//------------------------------------------------
+const SPTHandler=(request,response)=>{
+  spt();
+}
+//------------------------------------------------
+const pageNotFoundHandler=(request,response)=>{
+  response.writeHead(404, {'content-Type': contentTypeMapping[extension]})
+  response.end('<h1>Page Not Found</h1>')
+}
+
+module.exports={
+  homeHandler,
+  clientHandler,
+  SPTHandler,
+  pageNotFoundHandler,
+};
